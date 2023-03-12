097e49d9cde78e7b69e9c9ba46279256b75586fe
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-03-11 21:15:07 -0300

    feat: solarsystem

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" class='planets'>
+        <p data-testid="planet-name" class='headline'>{ `${planetName}` }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
---
+++ src/components/Title.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <div>
+        <h2 class='headline'>
+          { headline }
+        </h2>
+      </div>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
